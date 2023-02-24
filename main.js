9309d840c9b69a08cf9b409c20ff67cf97fcd412
sohandey80 <77267242+sohandey80@users.noreply.github.com>
2023-02-24 12:18:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(640,480);
+    canvas.position(450,250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color="";
+
+
+}
+function filter_tint()
+{
+    tint_color = document.getElementById("color_name").value;
+}
+function draw()
+{
+    image(video,0,0,640,480);
+    tint(tint_color);
+
+}
+function take_snapshot()
+{
+    save("rcb.png");
+}
